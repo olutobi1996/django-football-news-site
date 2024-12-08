08083f2944b4f8d94b693c3e1594308c368ead6c
olutobi Adebayo <olutobi1996@icloud.com>
2024-12-08 21:17:51 +0000

    changed debug back to true

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@)q)dmkw9a)kt%!t4pxqlp(_wi9owplt=!&1no0eb^)0)&#n52'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-olutobi1996-djangofootb-p47lxg23wkk.ws-eu117.gitpod.io',
 '.herokuapp.com']
