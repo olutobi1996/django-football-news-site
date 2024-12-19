90cdeb9026dd4e288e0bf2a367b63f31d0957798
olutobi Adebayo <olutobi1996@icloud.com>
2024-12-19 20:37:30 +0000

    admin class created

--- codestar/urls.py
+++ codestar/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
+    path('froala_editor/', include('froala_editor.urls')),
 ]
