714b1432ea3ab3510c713ae7f77400632c918995
olutobi Adebayo <olutobi1996@icloud.com>
2024-12-19 20:37:30 +0000

    admin class created

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Post
+from froala_editor.fields import FroalaField
 
-admin.site.register(Post)
-admin.site.register(Comment)
+
+@admin.register(Post)
+class PostAdmin(FroalaField.ModelAdmin):
+    list_display = ["title","author","created_on"]
+    list_display_links = ["title","created_on"]
+    search_fields = ["title"]
+    list_filter = ["created_on"]
+    prepopulated_fields = {'slug':('title',)} 
