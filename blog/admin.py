17460f2a20c04378dd471ef65c95695b96b0b176
olutobi Adebayo <olutobi1996@icloud.com>
2024-12-23 19:30:48 +0000

    edited problems from blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from .models import Post
-from froala_editor.fields import FroalaField
 
 
 @admin.register(Post)
-class PostAdmin(FroalaField.ModelAdmin):
+class PostAdmin(admin.ModelAdmin):
     list_display = ["title","author","created_on"]
     list_display_links = ["title","created_on"]
     search_fields = ["title"]
--- env.py
+++ env.py
@@ -2,4 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:qTzV6e4DfQRr@ep-flat-pine-a2k5ahlr.eu-central-1.aws.neon.tech/vowel_shush_marry_513117")
-os.environ.setdefault("SECRET_KEY", "<OdM08ZbYB)MJ>")
+os.environ.setdefault("SECRET_KEY", "OdM08ZbYB)MJ")
