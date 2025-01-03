89a00c92b7b8e67a389c289b239b67cc18fe3b12
olutobi Adebayo <olutobi1996@icloud.com>
2025-01-03 20:08:44 +0000

    added authentication

--- codestar/urls.py
+++ codestar/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("blog.urls"), name="blog-urls"),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('froala_editor/', include('froala_editor.urls')),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
