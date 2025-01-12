772815e4f54d1457d8eff990596872b26073bf08
olutobi Adebayo <olutobi1996@icloud.com>
2025-01-12 20:35:26 +0000

    imported cloudinary for info page

--- info/models.py
+++ info/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from froala_editor.fields import FroalaField
+from cloudinary.models import CloudinaryField
 
 class Info(models.Model):
     title = models.CharField(max_length=300, unique=True)
     slug = models.SlugField(max_length=300, unique=True)
+    profile_image = CloudinaryField('image', default='placeholder')
     content = FroalaField()
     def __str__(self):
         return self.name
