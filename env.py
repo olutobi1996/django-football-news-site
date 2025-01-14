0395bf7133ca5e65e1a30850ce6b70617800d11e
olutobi Adebayo <olutobi1996@icloud.com>
2025-01-14 20:04:29 +0000

    stop tracking env.py

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault(
-    "DATABASE_URL", "postgresql://neondb_owner:Gauo3Ts1cYbL@ep-dark-lake-a2awjdj6.eu-central-1.aws.neon.tech/coke_baggy_drab_972320")
-os.environ.setdefault("SECRET_KEY", "OdM08ZbYB)MJ")
-
-os.environ.setdefault(
-    "CLOUDINARY_URL", "cloudinary://295872683235164:Xj5ZNoOj8OcTVDYeZmp6EzZFtpg@dmxyx6qvy")
