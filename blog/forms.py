d69249af701c6b3b312e034c8960498e5b5c2471
olutobi Adebayo <olutobi1996@icloud.com>
2025-01-18 18:15:24 +0000

    fixed bugs in model, views, post_detail

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,4 @@ from django import forms
 class CommentForm(forms.ModelForm):
     class Meta:
         model = PostComment
-        fields = ['comment']  # Only include fields you want in the form
+        fields = ['comment']  
