948953154ba22ee45ad1e00523e98d5c3938b3b8
olutobi Adebayo <olutobi1996@icloud.com>
2025-01-19 19:23:34 +0000

    adjustments to base.html

--- codestar/settings.py
+++ codestar/settings.py
@@ -82,7 +82,7 @@ ROOT_URLCONF = 'codestar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],  # Correct path concatenation
+        'DIRS': [TEMPLATES_DIR],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
