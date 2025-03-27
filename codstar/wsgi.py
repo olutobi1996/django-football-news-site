bb6d194081c75514925dcf361caeacf34f5b858d
olutobi Adebayo <olutobi1996@icloud.com>
2025-03-27 10:18:14 +0000

    Fix: Include codestar module

--- codstar/wsgi.py
+++ codstar/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_football_news_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codstar.settings')
 
 application = get_wsgi_application()
